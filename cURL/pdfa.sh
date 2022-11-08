b15f77618aef5f68ecf2fb771817d9f855ab722e
Christopher Green <cgreen@datalogics.com>
2022-11-08 14:00:55 -0600

    Fix capitalization of the PDF/A version

--- cURL/pdfa.sh
+++ cURL/pdfa.sh
@@ -4,5 +4,5 @@ curl -X POST "https://api.pdfrest.com/pdfa" \
   -H "Api-Key: xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" \
   -F "file=@../Sample_Input/ducky.pdf" \
   -F "output=example_out" \
-  -F "output_type=pdf/a-1b" \
+  -F "output_type=PDF/A-1b" \
   -F "rasterize_if_errors_encountered=off"
