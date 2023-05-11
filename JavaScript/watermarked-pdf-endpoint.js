8df1c43aeb506885129c9b0d53a8a1880ea0427c
Charles MCGregory <charlesm@datalogics.com>
2023-05-11 17:17:55 -0500

    Update more samples.

--- JavaScript/watermarked-pdf-endpoint.js
+++ JavaScript/watermarked-pdf-endpoint.js
@@ -1,20 +1,43 @@
-import fetch, { FormData, fileFromSync } from "node-fetch";
+/**
+ * This request demonstrates how to apply a text watermark to a PDF.
+ * Horizontal and vertical offsets of the watermark are measured in PDF units. (1 inch = 72 PDF units)
+ */
+var axios = require('axios');
+var FormData = require('form-data');
+var fs = require('fs');
 
-let formdata = new FormData();
-formdata.append("file", fileFromSync("../Sample_Input/ducky.pdf"));
-formdata.append("watermark_text", "watermark");
-formdata.append("output", "example_out");
+// Create a new form data instance and append the PDF file and parameters to it
+var data = new FormData();
+data.append('file', fs.createReadStream('/path/to/file'));
+data.append('watermark_text', 'Hello, watermarked world!');
+data.append('font', 'Arial');
+data.append('text_size', '72');
+data.append('text_color_rgb', '255,0,0');
+data.append('opacity', '0.5');
+data.append('x', '0');
+data.append('y', '0');
+data.append('rotation', '0');
+data.append('output', 'pdfrest_watermarked_pdf');
 
-let requestOptions = {
-  method: "POST",
-  headers: {
-    "Api-Key": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", // Place your api key here
+// define configuration options for axios request
+var config = {
+  method: 'post',
+  maxBodyLength: Infinity, // set maximum length of the request body
+  url: 'https://api.pdfrest.com/unrestricted-pdf', 
+  headers: { 
+    'Api-Key': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', // Replace with your API key
+    ...data.getHeaders() // set headers for the request
   },
-  body: formdata,
-  redirect: "follow",
+  data : data // set the data to be sent with the request
 };
 
-fetch("https://api.pdfrest.com/watermarked-pdf", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
+// send request and handle response or error
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error); 
+});
+
+// If you would like to download the file instead of getting the JSON response, please see the 'get-resource-id-endpoint.js' sample.
