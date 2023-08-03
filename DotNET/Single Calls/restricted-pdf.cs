e04b706164822844641225ee79b35c34e7363053
Taylor Smith <tsmith@datalogics.com>
2023-08-03 15:27:38 -0500

    Re-add DotNET

---
+++ DotNET/Single Calls/restricted-pdf.cs
@@ -0,0 +1,33 @@
+using System.Text;
+
+using (var httpClient = new HttpClient { BaseAddress = new Uri("https://api.pdfrest.com") })
+{
+    using (var request = new HttpRequestMessage(HttpMethod.Post, "restricted-pdf"))
+    {
+        request.Headers.TryAddWithoutValidation("Api-Key", "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx");
+        request.Headers.Accept.Add(new("application/json"));
+        var multipartContent = new MultipartFormDataContent();
+
+        var byteArray = File.ReadAllBytes("/path/to/file");
+        var byteAryContent = new ByteArrayContent(byteArray);
+        multipartContent.Add(byteAryContent, "file", "file_name");
+        byteAryContent.Headers.TryAddWithoutValidation("Content-Type", "application/pdf");
+
+        var byteArrayOption = new ByteArrayContent(Encoding.UTF8.GetBytes("password"));
+        multipartContent.Add(byteArrayOption, "new_permissions_password");
+
+        var byteArrayOption2 = new ByteArrayContent(Encoding.UTF8.GetBytes("copy_content"));
+        multipartContent.Add(byteArrayOption2, "restrictions[]");
+        var byteArrayOption3 = new ByteArrayContent(Encoding.UTF8.GetBytes("edit_content"));
+        multipartContent.Add(byteArrayOption3, "restrictions[]");
+
+
+        request.Content = multipartContent;
+        var response = await httpClient.SendAsync(request);
+
+        var apiResult = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine("API response received.");
+        Console.WriteLine(apiResult);
+    }
+}
