99fcbe17fd3f96f89cfc6ae2aef7b9f63de3b21b
Christopher Green <cgreen@datalogics.com>
2024-06-06 13:30:17 -0500

    Update request-status.sh cURL sample

--- cURL/Endpoint Examples/Multipart Payload/request-status.sh
+++ cURL/Endpoint Examples/Multipart Payload/request-status.sh
@@ -1,2 +1,16 @@
-curl -X GET "https://api.pdfrest.com/request-status/xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" \
-  -H "Api-Key: xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+#!/bin/sh
+
+API_KEY="xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+REQUEST_ID=$(curl -X POST "https://api.pdfrest.com/pdfa" \
+  -H "Accept: application/json" \
+  -H "Content-Type: multipart/form-data" \
+  -H "Api-Key: $API_KEY" \
+  -H "response-type: requestId" \
+  -F "file=@/path/to/file.pdf" \
+  -F "output_type=PDF/A-3b" \
+  -F "output=example_out" \
+  | jq -r '.requestId')
+
+curl -X GET "https://api.pdfrest.com/request-status/$REQUEST_ID" \
+  -H "Api-Key: $API_KEY"
