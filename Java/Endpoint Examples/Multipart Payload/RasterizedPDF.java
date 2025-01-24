0b157ce4bf6afce8b96514162ad06ecf2ed17ee3
Eric Shore <erics@datalogics.com>
2025-01-24 13:36:20 -0600

    Update and rename RasterizedPdf.java to RasterizedPDF.java
    Fix file name and class to match conventions

--- Java/Endpoint Examples/Multipart Payload/RasterizedPDF.java
+++ Java/Endpoint Examples/Multipart Payload/RasterizedPDF.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.*;
 import org.json.JSONObject;
 
-public class RasterizedPdf {
+public class RasterizedPDF {
 
   // Specify the path to your file here, or as the first argument when running the program.
   private static final String DEFAULT_FILE_PATH = "/path/to/file.pdf";
